7223d8fd8dab4a4a75a7a282365e69975e3c76f1
Sun Xiaoyu <yinuo42@outlook.com>
2024-11-14 19:05:41 +0800

    modify model

---
+++ model/basic.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from torch import nn
+
+class CNN3(nn.Module):
+    def __init__(self, embeddim=256, kernel_sizes=7, kernel_strides=3, channels=(4, 16, 32), dropouts=0.1):
+        super(CNN3, self).__init__()
+        if isinstance(dropouts, float):
+            dropouts = [dropouts]*3
+        if isinstance(channels, int):
+            channels = [channels]*3
+        if isinstance(kernel_sizes, int):
+            kernel_sizes = [kernel_sizes]*3
+        if isinstance(kernel_strides, int):
+            kernel_strides = [kernel_strides]*3
+        self.backbone = nn.Sequential(
+            nn.Conv1d(1, channels[0], kernel_sizes[0], kernel_strides[0]),
+            nn.BatchNorm1d(channels[0]),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            nn.Dropout(dropouts[0]),
+            
+            nn.Conv1d(channels[0], channels[1], kernel_sizes[1], kernel_strides[1]),
+            nn.BatchNorm1d(channels[1]),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            nn.Dropout(dropouts[1]),
+            
+            nn.Conv1d(channels[1], channels[2], kernel_sizes[2], kernel_strides[2]),
+            nn.BatchNorm1d(channels[2]),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            nn.Dropout(dropouts[2]),
+            
+            nn.Flatten(),
+            nn.Linear(10 * channels[2], embeddim)
+        )
+    
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
+    
+    
+
+
+
--- model/load.py
+++ model/load.py
@@ -1,9 +1,48 @@
-from .cnn3 import CNNContrast, CNNSupervised, CNN3
+import torch
+from torch import nn
+from model.basic import CNN3
+
 
 def load_model(model_name, task, embeddim):
     if task == 'contrast':
         if model_name == 'cnn3':
-            return CNNContrast(network=CNN3, embeddim=embeddim)
+            return ContrastModel(network=CNN3, embeddim=embeddim)
     elif task == 'supervised':
         if model_name == 'cnn3':
-            return CNNSupervised(network=CNN3, embeddim=embeddim)
+            return SupervisedModel(network=CNN3, embeddim=embeddim)
+        
+
+class ContrastModel(nn.Module):
+    def __init__(self, network, embeddim=256, kernel_sizes=7, kernel_strides=3,
+                 channels=(4, 16, 32), dropouts=0.1):
+        super(ContrastModel,self).__init__()
+        self.embeddim = embeddim
+        self.encoder = network(embeddim, kernel_sizes, kernel_strides, channels, dropouts)
+        
+        
+    def forward(self,x):
+        """
+        Args:
+            x (torch.Tensor): inputs with N views (BxNxCxS)
+        Returns:
+            h (torch.Tensor): latent embedding for each of the N views (NxBxH)
+        """
+        nviews = x.shape[1]
+        x = x.permute(1, 0, 2, 3)
+        h = [self.encoder(x[n, ...]) for n in range(nviews)]
+        return torch.stack(h, dim=0)
+    
+
+class SupervisedModel(nn.Module):
+    def __init__(self, network, num_classes=4, embeddim=256, kernel_sizes=7, kernel_strides=3,
+                 channels=(4, 16, 32), dropouts=0.1):
+        super(SupervisedModel,self).__init__()
+        self.embeddim = embeddim
+        self.encoder = network(embeddim, kernel_sizes, kernel_strides, channels, dropouts)
+        self.fc = nn.Linear(embeddim, num_classes)
+        
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.fc(x)
+        return x
